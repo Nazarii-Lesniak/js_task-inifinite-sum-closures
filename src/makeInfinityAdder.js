668de6dcf336606b166a15e558462ae5d27a7a79
Nazarii Lesniak <naziksay@gmail.com>
2025-12-29 09:48:13 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let totalSum = 0;
+
+  return function adder(number) {
+    if (number === undefined) {
+      const FINAL_RESULT = totalSum;
+
+      totalSum = 0;
+
+      return FINAL_RESULT;
+    }
+
+    totalSum += number;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
